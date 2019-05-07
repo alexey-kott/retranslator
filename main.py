6de083b4788f4d17c69843a66dcd9b7fb4bdc73f
Alex Kott <alexey.kott@gmail.com>
2019-05-07 16:06:18 +0300

    ...

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import asyncio
 import json
-from typing import List, Dict
+import re
+from asyncio import sleep
+from typing import List, Dict, Union
 
 from socks import SOCKS5
 from telethon import TelegramClient
 from telethon.events import NewMessage
 from telethon.tl.custom import Message
+from nltk.corpus import stopwords
+from pymystem3 import Mystem
+from string import punctuation
+
+from telethon.tl.types import User, Channel
 
 from config import APP_API_HASH, APP_API_ID, PHONE_NUMBER, SETTINGS_FILE
 from utils import init_logger
 
 logger = init_logger()
-
 
 # proxy = None
 proxy = (SOCKS5, '51.144.86.230', 18001, True, 'usrTELE', 'avt231407')
@@ -27,17 +33,55 @@ def get_settings() -> Dict:
         return json.load(file)
 
 
-def is_word_matching(message: Message, words: List[str]) -> bool:
-    
+def log_message(message: Message):
+    with open("messages.log", "a") as file:
+        sender: User = message.sender
+        if sender.username:
+            sender_username = f"@{sender.username}"
+        else:
+            sender_username = ''
+        sender_full_name = f"{sender.first_name}{' ' + sender.last_name if sender.last_name else ''} {sender_username}"
+        file.write(f"{sender_full_name} {message.date} \n{message.text}\n\n")
+
+
+def get_lemmatized_tokens(text: str) -> List[str]:
+    mystem = Mystem()
+    russian_stopwords = stopwords.words("russian")
+
+    tokens = mystem.lemmatize(text.lower())
+    tokens = [token for token in tokens if
+              token not in russian_stopwords and token != " " and token.strip() not in punctuation]
+
+    return tokens
+
+
+def is_word_matching(text: str, words: List[str]) -> bool:
+    lemmatized_tokens = get_lemmatized_tokens(text)
+    if set(lemmatized_tokens).intersection(set(words)):
+        return True
+
     return False
 
 
-def is_regexp_matching(message: Message, regexps: str) -> bool:
+def is_regexp_matching(text: str, regexps: str) -> bool:
+    for regexp in regexps:
+        if re.findall(regexp, text):
+            return True
 
     return False
 
 
 def is_sender_matching(message: Message, users: str) -> bool:
+    for user in users:
+        if user.startswith('@'):
+            sender_username = message.sender.username.lower()
+            if f"@{sender_username}" == user:
+                return True
+        else:
+            sender: User = message.sender
+            sender_name = f"{sender.first_name}{' ' + sender.last_name if sender.last_name else ''}"
+            if sender_name == user:
+                return True
 
     return False
 
@@ -50,7 +94,7 @@ def check_message(message: Message) -> bool:
             if is_word_matching(text, filter_items):
                 return True
         if filter_type == 'REGEXP':
-            if is_regexp_matching(message, filter_items):
+            if is_regexp_matching(text, filter_items):
                 return True
         if filter_type == 'USERS':
             if is_sender_matching(message, filter_items):
@@ -59,18 +103,32 @@ def check_message(message: Message) -> bool:
     return False
 
 
-def get_receivers() -> List:
-    pass
+async def get_receivers() -> List[Union[User, Channel]]:
+    receiver_entities = []
+    settings = get_settings()
+    receiver_usernames = settings['RECEIVERS']
+    for receiver_username in receiver_usernames:
+        entity = await client.get_entity(receiver_username)
+        receiver_entities.append(entity)
+        await sleep(1)
+
+    return receiver_entities
 
 
 async def send_out_message(message: Message) -> None:
-    receivers = get_receivers()
+    receivers = await get_receivers()
+
+    for receiver in receivers:
+        if receiver.username == message.sender.username:
+            continue
+        await client.forward_messages(receiver, message)
+        await sleep(1)
 
 
 @client.on(NewMessage)
 async def new_message_handler(event: NewMessage.Event):
     message = event.message
-    print(message)
+    log_message(message)
     if check_message(message):
         await send_out_message(message)
 
